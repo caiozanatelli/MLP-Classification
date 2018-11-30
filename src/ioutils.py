c96b1ac25010abf2248a7e4c859513d357202c21
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2018-11-30 12:48:17 -0200

    Starting project

---
+++ src/ioutils.py
@@ -0,0 +1,60 @@
+import numpy as np
+import sys
+
+class Singleton(type):
+
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance
+
+class IOUtils(object):
+    '''
+    A generic class for input and output manipulation.
+    '''
+    __metaclass__ = Singleton
+    __logfile = None
+
+    def __init__(self, logpath=None):
+        if not logpath is None:
+            self.open_log(logpath)
+
+    def read_input(self, filepath):
+        '''
+        Read a csv input that contains 17 features and 1 class
+
+        Arguments:
+            [str] -- path of the input file.
+
+        Returns:
+            [numpy] -- np matrix
+        '''
+        matrix = np.loadtxt(filepath, dtype=object, delimiter=',')
+        index  = np.where(matrix == 'class')
+        column = int(index[1])
+        if column != len(matrix[0]) - 1:
+            matrix[:, [column, -1]] = matrix[:, [-1, column]]
+        return matrix
+    
+    def save_data(self, data):
+        '''
+        Save a string data to the log file
+
+        Arguments:
+            [str] -- the string to be stored
+        '''
+        if self.__logfile is None:
+            print('Log file not opened.')
+        else:
+            self.__logfile.write(data + '\n')
+
+    def open_log(self, filepath):
+        '''
+        Open the log file for storing partial results
+        
+        Arguments:
+            [str] -- path of a file for logging purposes
+        '''
+        self.__logfile = open(filepath, 'w')
---
+++ src/main.py
@@ -0,0 +1,51 @@
+import argparse
+import numpy as np
+from time import time
+from os import getcwd
+#from nn import NeuralNet
+from os.path import join
+from os.path import split
+from keras.utils import np_utils
+from ioutils import IOUtils
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Galaxy Classifier using Neural Networks')
+    parser.add_argument('-i', '--input', action='store', type=str, required=True,
+                        help='Input file path')
+    parser.add_argument('-o', '--output', action='store', type=str, required=True,
+                        help='Output file path')
+    parser.add_argument('-s', '--seed', action='store', type=int, default=1,
+                        help='Seed for random numbers generation')
+    parser.add_argument('-n', '--neurons', action='store', type=int, default=17,
+                        help='Number of neurons in each hidden layer')
+    parser.add_argument('--hidden', action='store', type=int, default=1,
+                        help='Number of hidden layers in the network')
+    parser.add_argument('-a', '--activation', action='store', type=str,
+                        choices=['relu', 'sigmoid', 'softmax'], default='relu',
+                        help='Activation function for the neurons')
+    parser.add_argument('-e', '--epoch', action='store', type=int, default=100,
+                        help='Number of epoches for the network')
+    parser.add_argument('-b', '--batch', action='store', type=int, default=10,
+                        help='Batch size for the training phase')
+    parser.add_argument('-l', '--learning-rate', action='store', type=float, default=0.01,
+                        help='Network\' learning rate')
+    parser.add_argument('-d', '--decay', action='store', type=float, default=0.00005,
+                        help='Decay factor for the learning rate')
+    
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    ioutils = IOUtils()
+    classes_dict = {'GALAXY':0, 'STAR':1, 'QSO':2}
+    matrix   = ioutils.read_input(args.input)
+    classes  = matrix[1:, -1]
+    nclasses = len(classes_dict.keys())
+    X = np.array(matrix[1:, :-1], dtype=float)
+    examples, attributes = X.shape
+
+    #TODO: One-Hot encoding
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
